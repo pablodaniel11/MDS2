f6db277cca4816ae5ee2e5a04be02ec5e170e0c5
pablodaniel11 <56073003+pablodaniel11@users.noreply.github.com>
2020-03-16 18:26:41 +0100

    Add files via upload

---
+++ Foro_UAL/src/main/java/com/example/Foro_UAL/Ventana_inicio.java
@@ -0,0 +1,47 @@
+package com.example.Foro_UAL;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class Ventana_inicio extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener(e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "Ventana_inicioServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = Ventana_inicio.class, productionMode = false)
+    public static class Ventana_inicioServlet extends VaadinServlet {
+    }
+}
